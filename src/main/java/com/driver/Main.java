1b34db01d5bcf8ca238548d3592b5cea2b1aafcc
Vinit karki <vinitkarki29797@gmail.com>
2022-10-05 12:27:47 +0530

    completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj=new RWOnly();
+
+    //Before getter and setter
+    //obj.name=12;                     //Error-- The field RWOnly.name is not visible
+    //System.out.println(obj.name);    //Error-- The field RWOnly.name is not visible
+
+    //after getter and setter
+    obj.setName(15);
+    System.out.println(obj.getName());
+
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int name=10;
+
+    public int getName() {
+        return name;
+    }
+    public void setName(int name) {
+        this.name = name;
+    }
 }
